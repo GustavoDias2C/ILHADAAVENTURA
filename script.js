acf644923258a6596bf871e5fa5b0f917d907dd2
Gustavo Dias 2C <00001132654634sp@al.educacao.sp.gov.br>
2024-09-13 08:21:17 -0300

    14

---
+++ script.js
@@ -0,0 +1,8 @@
+const avanca = document.querySelectorAll('.btn-proximo')
+
+avanca.forEach(button => {
+    button.addEventListene('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo'
+    })
+})
